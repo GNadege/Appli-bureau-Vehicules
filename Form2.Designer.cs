666aaaabfd6f76f296c896df59a2c86b44a88fe1
GNadege <nadege.grandyot@gmail.com>
2023-05-13 18:49:03 +0200

    master_file

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -33,12 +33,30 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.mdpTXT = new System.Windows.Forms.TextBox();
+            this.loginTXT = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.fonctionTXT = new System.Windows.Forms.TextBox();
+            this.descriptionTXT = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(35, 84);
+            this.button2.Location = new System.Drawing.Point(12, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(112, 34);
             this.button2.TabIndex = 0;
@@ -48,7 +66,8 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(310, 407);
+            this.button1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.button1.Location = new System.Drawing.Point(412, 139);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(112, 34);
             this.button1.TabIndex = 1;
@@ -58,17 +77,18 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(310, 263);
+            this.button3.Location = new System.Drawing.Point(412, 81);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(112, 34);
             this.button3.TabIndex = 2;
-            this.button3.Text = "Afficher";
+            this.button3.Text = "Actualiser";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(310, 205);
+            this.button4.ForeColor = System.Drawing.Color.DarkRed;
+            this.button4.Location = new System.Drawing.Point(412, 253);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(112, 34);
             this.button4.TabIndex = 3;
@@ -79,16 +99,190 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(475, 19);
+            this.dataGridView1.Location = new System.Drawing.Point(545, 12);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 33;
-            this.dataGridView1.Size = new System.Drawing.Size(465, 454);
+            this.dataGridView1.Size = new System.Drawing.Size(1256, 734);
             this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(138, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(112, 34);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Réservations";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(412, 197);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(112, 34);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Modifier";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Nom";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(138, 84);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 31);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(138, 151);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(150, 31);
+            this.textBox2.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 25);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Prénom";
+            // 
+            // mdpTXT
+            // 
+            this.mdpTXT.Location = new System.Drawing.Point(138, 271);
+            this.mdpTXT.Name = "mdpTXT";
+            this.mdpTXT.Size = new System.Drawing.Size(150, 31);
+            this.mdpTXT.TabIndex = 11;
+            // 
+            // loginTXT
+            // 
+            this.loginTXT.Location = new System.Drawing.Point(138, 212);
+            this.loginTXT.Name = "loginTXT";
+            this.loginTXT.Size = new System.Drawing.Size(150, 31);
+            this.loginTXT.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 277);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 25);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Mot de passe";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 25);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Login";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 337);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 25);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Fonction";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(22, 400);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(102, 25);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Description";
+            // 
+            // fonctionTXT
+            // 
+            this.fonctionTXT.Location = new System.Drawing.Point(138, 331);
+            this.fonctionTXT.Name = "fonctionTXT";
+            this.fonctionTXT.Size = new System.Drawing.Size(150, 31);
+            this.fonctionTXT.TabIndex = 17;
+            // 
+            // descriptionTXT
+            // 
+            this.descriptionTXT.Location = new System.Drawing.Point(138, 400);
+            this.descriptionTXT.Name = "descriptionTXT";
+            this.descriptionTXT.Size = new System.Drawing.Size(150, 31);
+            this.descriptionTXT.TabIndex = 18;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label7.Location = new System.Drawing.Point(256, 17);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(123, 25);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "UTILISATEURS";
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button7.Location = new System.Drawing.Point(396, 12);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(128, 34);
+            this.button7.TabIndex = 20;
+            this.button7.Text = "Déconnexion";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 550);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(479, 25);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "Assurez vous que ja ligne entère soit séléctionnées lorsque ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 575);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(318, 25);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "vous souhaitez modifier ou supprimer.";
             // 
             // Form2
             // 
-            this.ClientSize = new System.Drawing.Size(953, 486);
+            this.ClientSize = new System.Drawing.Size(1813, 758);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.descriptionTXT);
+            this.Controls.Add(this.fonctionTXT);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.loginTXT);
+            this.Controls.Add(this.mdpTXT);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -97,6 +291,7 @@
             this.Name = "Form2";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -108,5 +303,23 @@
         private Button button3;
         private Button button4;
         private DataGridView dataGridView1;
+        private Button button5;
+        private Button button6;
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label2;
+        private TextBox mdpTXT;
+        private TextBox loginTXT;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox fonctionTXT;
+        private TextBox descriptionTXT;
+        private Label label7;
+        private Button button7;
+        private Label label8;
+        private Label label9;
     }
 }
--- Form2.cs
+++ Form2.cs
@@ -1,4 +1,5 @@
-﻿using MySql.Data.MySqlClient;
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -9,23 +10,42 @@ using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Security.Cryptography;
+using System.Runtime.Intrinsics.Arm;
+using System.Reflection;
 
 namespace ConnexionTable
 {
     public partial class Form2 : Form
     {
-        private MySqlConnection conn;
+        private MySqlConnection conn; //informations de conexion a la base de données
 
-        private string server = "localhost";
-        private string database = "bdd-sn";
-        private string uid = "root";
-        private string password = "";
+        private string server = "172.16.0.9";
+        private string database = "bdd_sn";
+        private string user = "User1";
+        private string password = "Prevert-77";
 
-        public Form2(/*MySqlConnection conn*/)
+        public Form2()
         {
-            InitializeComponent();
-            /*conn = new MySqlConnection($"Server={server};Database={database};Uid={uid};Pwd={password};");
-            this.conn = conn;*/
+            InitializeComponent();//initialisation des composants
+            conn = new MySqlConnection($"Server={server};Database={database};User={user};Pwd={password};");//création de la connection avec requette sql
+            try  //Permet l'affichage automatique de la base
+            {
+                conn.Open();
+                String query = "Select idadmin, login, pwd, fonction, Description, nom, prenom from admin"; //Récupéraion du contenu de la table
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0].DefaultView;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erreur de connexion à la base de données : {ex.Message}"); //Message d'erreur en cas d'impossibilité de récuperer le contenu
+            }
         }
 
 
@@ -53,13 +73,24 @@ namespace ConnexionTable
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string mdp = mdpTXT.Text + "pepper"; //cryptage 
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(mdp));
+                var sb = new StringBuilder(hash.Length * 2);
+                foreach (byte b in hash)
+                {
+                    sb.Append(b.ToString("x2"));
+                }
+                mdp = sb.ToString();
+            }
             try
             {
                 conn.Open();
-                //string query = $"INSERT INTO user (nom, prenom, adresse) VALUES ('{nom}', '{prenom}', '{adresse}');";
-                // MySqlCommand cmd = new MySqlCommand(query, conn);
-                //int rowsAffected = cmd.ExecuteNonQuery();
-                //MessageBox.Show($"Enregistrement effectué, {rowsAffected} ligne(s) affectée(s).", "Succès");
+                string query = $"INSERT INTO admin (login, pwd, fonction, Description, nom, prenom) VALUES ('"+loginTXT.Text+"','"+mdp+"','"+fonctionTXT.Text+"','"+descriptionTXT.Text+"','"+textBox1.Text+"','"+textBox2.Text+"')";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                int rowsAffected = cmd.ExecuteNonQuery();
+                MessageBox.Show($"Ajout éffectué avec succès");
             }
             catch (Exception ex)
             {
@@ -68,24 +99,24 @@ namespace ConnexionTable
             finally
             {
                 conn.Close();
+                this.button3_Click(sender, e);
             }
+            
         }
-        public Form2()
-        {
-            InitializeComponent();
-        }
+        
 
         private void button3_Click(object sender, EventArgs e)
         {
             try
             {
-                conne.Open();
-                String query = "Select * from user";
-                MySqlCommand cmd = new MySqlCommand(query, conne);
-                MySqlDataReader reader = cmd.ExecuteReader();
-                DataTable dt = new DataTable();
-                dt.Load(reader);
-                dataGridView1.DataSource = dt;
+                conn.Open();
+                String query = "Select idadmin, login, pwd, fonction, Description, nom, prenom from admin";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0].DefaultView;
 
             }
             catch (Exception ex)
@@ -94,7 +125,7 @@ namespace ConnexionTable
             }
             finally
             {
-                conn.Close();
+               conn.Close();
             }
         }
 
@@ -103,7 +134,7 @@ namespace ConnexionTable
             try
             {
                 conn.Open();
-                string query = "DELETE FROM user WHERE id=" + dataGridView1.SelectedCells[0].Value.ToString();
+                string query = "DELETE FROM admin WHERE idadmin=" + dataGridView1.SelectedCells[0].Value.ToString();
                 MySqlCommand cmd = new MySqlCommand(query, conn);
                 int i = cmd.ExecuteNonQuery();
                 if (i >= -1)
@@ -119,9 +150,86 @@ namespace ConnexionTable
             finally
             {
 
-                conne.Close();
-                this.affichBTN_Click(sender, e);
+                conn.Close();
+                this.button3_Click(sender, e);
             }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form4 form4 = new Form4();
+            form4.Show();
+            this.Hide();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string mdp = mdpTXT.Text + "pepper"; //cryptage 
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(mdp));
+                var sb = new StringBuilder(hash.Length * 2);
+                foreach (byte b in hash)
+                {
+                    sb.Append(b.ToString("x2"));
+                }
+                mdp = sb.ToString();
+            }
+            try
+            {
+                conn.Open();
+                
+                string query = "UPDATE admin SET login='" + loginTXT.Text + "', pwd='" + mdp + "', fonction='"+ fonctionTXT.Text+ "', description='"+ descriptionTXT.Text+ "', nom='"+textBox1.Text+"', prenom='"+textBox2.Text+"' WHERE idadmin=" + dataGridView1.SelectedCells[0].Value.ToString();
+                
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                int i = cmd.ExecuteNonQuery();
+                if (i >= -1)
+                {
+                    MessageBox.Show("Donnée modifiée avec succes!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erreur de connexion à la base de données : {ex.Message}");
+            }
+            finally
+            {
+
+                conn.Close();
+                this.button3_Click(sender, e);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Hide();
+                Form1 form1 = new Form1();
+
+                form1.Show();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erreur de connexion à la base de données : {ex.Message}");
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0) // make sure user select at least 1 row 
+            {
+                //textBox1.Text = dataGridView1.SelectedRows[0].Cells[5].Value + string.Empty;
+                //textBox2.Text = dataGridView1.SelectedRows[0].Cells[6].Value + string.Empty;
+             //textBox1.Text= Convert.ToString(dataGridView1[5, dataGridView1.SelectedRows].Value);
+            }
+            
         }
     }
 }
